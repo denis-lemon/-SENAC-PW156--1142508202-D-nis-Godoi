22a9013aa646b6b42e3e7a0faef20aee7bef3cfc
Denis Godoi <110983714+denis-lemon@users.noreply.github.com>
2022-11-23 08:41:41 -0300

    Add files via upload

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get('/teste', (req: Request, res: Response) => {
+    throw new Error("Erro ao fazer esta requisição")
+})
+export { router };
